0f0b5f18ddda159753f50bf9a217fcb6649001d3
justinbesteman <justin@besteman.io>
2016-10-24 22:27:28 -0400

    Logic complete

--- assets/javascript/help.js
+++ assets/javascript/help.js
@@ -23,19 +23,19 @@ $(document).ready(function() {
     var attack_power = [];
     var counter_power = [];
     //console.log(hero_image[0]);
-  
-  var enemy_select;
-  var hero_select;
+	
+	// Declared enemy_select in global scope
+	var enemy_select;
 
     function it_is_over_9000(){
         for (var i = 0; i < young_hero.length; i++) {
-            var x = Math.floor(Math.random()*20) + 3;
-            var y = Math.floor(Math.random()*10) + 3;
+            var x = Math.floor(Math.random(attack_power)*20) + 3;
+            var y = Math.floor(Math.random(attack_power)*10) + 3;
             attack_power.push(x);
             counter_power.push(y);
+            console.log("\nyoung_hero ID: "+young_hero[i]+"\nattack_power: "+x+"\ncounter_power: "+y);
         }
     }
-
 
     function ready_board(){
         console.log("ready board");
@@ -63,14 +63,14 @@ $(document).ready(function() {
         $(".hero_button").on("click", function() {
             console.log("\n---------------------------------------------- HERO SELECTED");
             var hero = $(this);
-            hero_select = hero.data('index');
-
+            var hero_select = hero.data('index');
             for (var i = 0; i < young_hero.length; i++) {
                 //var attack = ;
                 if (i != hero_select){
                     var enemies = $("<button>");
                     enemies.addClass("hero enemy");
                     enemies.attr({
+                        "data-power" : it_is_over_9000(),
                         "data-name": young_hero[i],
                         "data-health": health[i],
                         "data-image": hero_image[i],
@@ -91,12 +91,9 @@ $(document).ready(function() {
 
     function defender(){
         $(".enemy").on("click", function() {
-
-          if ($("#defend").is(":empty")) {
-
             var enemy = $(this);
-            enemy_select = enemy.data("index"); 
-
+            enemy_select = enemy.data("index");	// Removed the var, since declared in global scope...
+			console.log("\n---------------------------------------------- ENEMY SELECTED: "+enemy_select);
             for (var i = 0; i < young_hero.length; i++) {
                 if (i == enemy_select) {
                     var defender = $("<button>");
@@ -116,118 +113,51 @@ $(document).ready(function() {
                     $(this).remove();
                 }
             }
-
-          }
-
         });
-
-
-
     }
 
-
-    var count = young_hero.length - 1;
-    console.log(count);
-
-    function winner_winner_chickdinner(){
-      console.log("hey");
-      var winner_name = $(".hero_button").data("name")
-      $("#buttons").append(winner_name);
-    }
-
-    $(".defend_button").on("click" , function() {
-
-      console.log($("#battle").is(":empty"));
-
-       if ($("#defend").is(":empty") != true) {
-
+    $(".defend_button").on("click" , function(){
         console.log("---------------------------------------------- DEFEND button clicked");
-     
-        console.log("Hero counter points: "+$(".hero_button").data("counter"));
+        console.log("HEALTH: "+$(".defender").data("health"));
         console.log("Hero attack points: "+$(".hero_button").data("attack"));
-      
-        console.log("Defender counter points: "+$(".defender").data("counter"));
-        console.log("Defender attack points: "+$(".defender").data("attack"));
-
-
-        console.log(attack_power);
-        console.log(counter_power);
-
-        var hero_health = $(".hero_button").data("health") - $(".defender").data("attack");
-
-        var defender_health = $(".defender").data("health") - $(".hero_button").data("counter");
-
-        var hero_counter = $(".hero_button").data("counter") + 10;
-
-        console.log(hero_health);
-        console.log(defender_health);
-
-        $(".hero_button").data({
-            "health":  hero_health,
-            "counter": hero_counter
+        console.log("Defender health points: "+$(".defender").data("health"));
+		
+		var battleResult = $(".defender").data("health") - $(".hero_button").data("attack");
+		console.log("battleResult should be: "+battleResult );
+        
+		$(".defender").data({
+            "health":  battleResult
         });
-          
-      $(".defender").data({
-              "health":  defender_health
-          });
-          
-      console.log("---------------------------------------------- Updating button....");
-
-      var new_health_defender = $(".defender").data("health");
-      var new_health_hero = $(".hero_button").data("health");
-      var new_hero_counter = $(".hero_button").data("counter");
-      
-      var update_defender = $("<button>");
-
-          update_defender.addClass("hero defender");
-          update_defender.attr({
-              "data-name": young_hero[enemy_select],
-              "data-health": new_health_defender,  
-              "data-image": hero_image[enemy_select],
-              "data-attack": attack_power[enemy_select],
-              "data-counter": counter_power[enemy_select],
-              "data-index": enemy_select
-          });
-          update_defender.text(young_hero[enemy_select]);
-          update_defender.append(hero_image[enemy_select]);
-          update_defender.append(new_health_defender);
         
-        $("#defend").html(update_defender.data('name','health','image'));
-
-
-        //----------------------------------------------------------------
-
-          var update_hero = $("<button>");
-
-          update_hero.addClass("hero hero_button");
-          update_hero.attr({
-              "data-name": young_hero[hero_select],
-              "data-health": new_health_hero,  
-              "data-image": hero_image[hero_select],
-              "data-attack": attack_power[hero_select],
-              "data-counter": new_hero_counter,
-              "data-index": hero_select
-          });
-          update_hero.text(young_hero[hero_select]);
-          update_hero.append(hero_image[hero_select]);
-          update_hero.append(new_health_hero);
+		console.log("And is now: "+$(".defender").data("health"));
         
-        $("#buttons").html(update_hero.data('name','health','image'));
+        
+        console.log("---------------------------------------------- Updating button....");
+		var newHealth = $(".defender").data("health");
+		
+		var updateEnemyBtn = $("<button>");
+            updateEnemyBtn.addClass("hero defender");
+            updateEnemyBtn.attr({
+                "data-name": young_hero[enemy_select],
+                "data-health": newHealth,		// New health
+                "data-image": hero_image[enemy_select],
+                "data-attack": attack_power[enemy_select],
+                "data-counter": counter_power[enemy_select],
+                "data-index": enemy_select
+            });
+            updateEnemyBtn.text(young_hero[enemy_select]);
+            updateEnemyBtn.append(hero_image[enemy_select]);
+            updateEnemyBtn.append(newHealth);
+			
+			$("#defend").html(updateEnemyBtn.data('name','health','image'));
 
-     
-          if($(".defender").data("health") < 0){
+		// If health is ==0 OR BELOW! : Remove.
+        if($(".defender").data("health") <= 0){
             $(".defender").remove();
-            count--;
-          }
-           console.log(count);
-          if($(".hero_button").data("health") < 0){
-            $(".hero_button").remove();
-          }
-
-       } 
-  });
+			console.log("---------------------------------------------- Defender has been killed.")
+        }
+    });
 
     ready_board();
     char();
-
 });
